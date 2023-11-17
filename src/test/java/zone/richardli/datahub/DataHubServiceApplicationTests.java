d3b2aef04512edb14f71b9cb9390eef2206c5295
Yuyang Li <richard-yy.li@polyu.edu.hk>
2023-11-17 15:38:53 +0800

    First commit for Spring Boot project

---
+++ src/test/java/zone/richardli/datahub/DataHubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package zone.richardli.datahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataHubServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
