c825599efd9bacc8c90b18ef5be3246057ac384c
Yuyang Li <richard-yy.li@polyu.edu.hk>
2023-12-05 12:59:48 +0800

    Add: CORS configuration for front-end linkage

---
+++ src/main/java/zone/richardli/datahub/advise/CORSControllerAdvise.java
@@ -0,0 +1,21 @@
+package zone.richardli.datahub.advise;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@ControllerAdvice
+public class CORSControllerAdvise {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer () {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+
+}
